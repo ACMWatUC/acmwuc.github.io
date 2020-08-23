339063f13c2ef4a406f8559c52d377b1889633a8
linakaval <56026598+linakaval@users.noreply.github.com>
2020-08-23 12:05:39 -0400

    Source files for the new site
    When we want to deploy the site, do gatsby build and copy the new folder into master branch

---
+++ src/pages/Resources.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ResourcesPage = () => (
+    <Layout>
+        <SEO title="Resources" />
+        <h1>More information coming soon!</h1>
+    </Layout>
+)
+
+export default ResourcesPage
