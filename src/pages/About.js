8a0d702ef1f4ac8d5976c1f124e8dc5ca9e41ca3
linakaval <56026598+linakaval@users.noreply.github.com>
2021-01-21 18:38:03 -0500

    this is the source code of acmwuc.github.io as of 1/21/21

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
 import { graphql } from "gatsby"
-import Image from 'src/components/Image'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
